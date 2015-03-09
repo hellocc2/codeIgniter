ced743bec60c532f869cef56f4c1a681d49f444d
jiangcai <jiangcai@milanoo.com>
2015-03-09 10:27:45 +0800

    del

--- application/views/partials/banner.php
+++
@@ -1,5 +0,0 @@
-<div id="sims-banner" class="owl-carousel owl-theme">
-	<div class="item"><img src="<?php echo base_url() ?>assets/img/fullimage1.jpg" alt="The Last of us"></div>
-	<div class="item"><img src="<?php echo base_url() ?>assets/img/fullimage2.jpg" alt="GTA V"></div>
-	<div class="item"><img src="<?php echo base_url() ?>assets/img/fullimage3.jpg" alt="Mirror Edge"></div>	
-</div>
