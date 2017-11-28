0befbe8d9d4607caf8f7a2ba519aa81f908419e2
hellocc2 <jiang163cai@gmail.com>
2017-11-28 14:00:58 +0800

    cart

---
+++ storage/cache/2c/2c630b9b6e45ba04467539f369b5866e59b83e6a0e846cb08f716fffcc8d977c.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_e96422a54ce4c76531a69361fadf08f8eeae84c4d3119ef9ff5792debd24e5f6 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
---
+++ upload/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://v2.cart.com/admin/');
+define('HTTP_CATALOG', 'http://v2.cart.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://v2.cart.com/admin/');
+define('HTTPS_CATALOG', 'http://v2.cart.com/');
+
+// DIR
+define('DIR_APPLICATION', '/data/cart/upload/admin/');
+define('DIR_SYSTEM', '/data/cart/upload/system/');
+define('DIR_IMAGE', '/data/cart/upload/image/');
+define('DIR_STORAGE', '/data/cart/storage/');
+define('DIR_CATALOG', '/data/cart/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '192.168.68.129');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'hishop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
